b619400203c561e5970cb177c7c7c0f79cd0a2ab
Ben Murray <ben.murray@axientcorp.com>
2022-05-25 16:18:33 -0500

    Add other ghosts

--- scratch/anim.py
+++ scratch/anim.py
@@ -2,11 +2,11 @@ import pygame, sys
 
 
 class Player(pygame.sprite.Sprite):
-    FRAME_DELTA = 0.06
+    FRAME_DELTA = 0.12
 
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, color='purple', pos_x=0, pos_y=0):
         super().__init__()
-        self.sprite_sheet = pygame.image.load('../assets/images/purple_ghost.png').convert()
+        self.sprite_sheet = pygame.image.load(f'../assets/images/{color}_ghost.png').convert()
         self.image = pygame.Surface((125, 125))
         self.image.blit(self.sprite_sheet, dest=(0, 0), area=(0, 0, 125, 125))
 
@@ -18,6 +18,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.topleft = [pos_x, pos_y]
 
         self.frame_num = 0
+        self.frame_delta = Player.FRAME_DELTA
 
     def create_loops(self):
         front1 = pygame.Surface((125, 125))
@@ -59,7 +60,7 @@ class Player(pygame.sprite.Sprite):
         return front_loop, left_loop, right_loop, up_loop, front_loop
 
     def update(self):
-        self.frame_num += Player.FRAME_DELTA
+        self.frame_num += self.frame_delta
         if self.frame_num >= len(self.current_sprite_loop):
             self.frame_num = 0
 
@@ -67,23 +68,28 @@ class Player(pygame.sprite.Sprite):
 
     def stop_moving(self):
         self.current_sprite_loop = self.idle
+        self.frame_delta = 0.06
 
     def move_up(self):
+        self.frame_delta = Player.FRAME_DELTA
         self.current_sprite_loop = self.up
         self.pos = (self.pos[0], self.pos[1] - 2)
         self.rect.topleft = [self.pos[0], self.pos[1]]
 
     def move_down(self):
+        self.frame_delta = Player.FRAME_DELTA
         self.current_sprite_loop = self.down
         self.pos = (self.pos[0], self.pos[1] + 2)
         self.rect.topleft = [self.pos[0], self.pos[1]]
 
     def move_left(self):
+        self.frame_delta = Player.FRAME_DELTA
         self.current_sprite_loop = self.left
         self.pos = (self.pos[0] - 2, self.pos[1])
         self.rect.topleft = [self.pos[0], self.pos[1]]
 
     def move_right(self):
+        self.frame_delta = Player.FRAME_DELTA
         self.current_sprite_loop = self.right
         self.pos = (self.pos[0] + 2, self.pos[1])
         self.rect.topleft = [self.pos[0], self.pos[1]]
@@ -98,8 +104,16 @@ pygame.display.set_caption("Animation")
 
 
 moving_sprintes = pygame.sprite.Group()
-player = Player(100, 100)
+player = Player('purple', 100, 100)
+player2 = Player('red', 250, 100)
+player3 = Player('blue', 400, 100)
+player4 = Player('green', 550, 100)
+player5 = Player('slime', 700, 100)
 moving_sprintes.add(player)
+moving_sprintes.add(player2)
+moving_sprintes.add(player3)
+moving_sprintes.add(player4)
+moving_sprintes.add(player5)
 
 while True:
     for event in pygame.event.get():
@@ -111,14 +125,34 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_DOWN]:
         player.move_down()
+        player2.move_down()
+        player3.move_down()
+        player4.move_down()
+        player5.move_down()
     elif keys[pygame.K_UP]:
         player.move_up()
+        player2.move_up()
+        player3.move_up()
+        player4.move_up()
+        player5.move_up()
     elif keys[pygame.K_LEFT]:
         player.move_left()
+        player2.move_left()
+        player3.move_left()
+        player4.move_left()
+        player5.move_left()
     elif keys[pygame.K_RIGHT]:
         player.move_right()
+        player2.move_right()
+        player3.move_right()
+        player4.move_right()
+        player5.move_right()
     else:
         player.stop_moving()
+        player2.stop_moving()
+        player3.stop_moving()
+        player4.stop_moving()
+        player5.stop_moving()
 
     # Drawing
     screen.fill((0, 0, 0))
